3b56248363f34edd2ac909b5a5726e8929ecbd2e
Naveen-8218 <nc319631@gmail.com>
2025-06-27 17:46:38 +0530

    Initial commit - Navbar component added

--- src/component/Pages/loginsignup.jsx
+++ src/component/Pages/loginsignup.jsx
@@ -3,8 +3,8 @@ import log from '../../assets/login.webp'
 
 const Loginsignup = () => {
   return (
-    <div className=' h-130  p-10' style={{ backgroundImage: `url(${log})`, width:`214vh` }}>
-      <div  className='w-100  h-100 border-2 flex ml-90 flex-col justify-center items-center gap-8  text-white rounded-xl hover:scale-105 '>
+    <div className=' h-full  p-10 flex justify-center' style={{ backgroundImage: `url(${log})`, width:`214vh` }} >
+      <div  className='w-100  h-100 border-2 flex  flex-col justify-center items-center gap-8  text-white rounded-xl hover:scale-105 '>
           <h1 className='mr-25 font-bold text-2xl'>Sign Up</h1>
 
           <div className='w-80% flex flex-col gap-8'>
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -9,36 +9,38 @@ import { IoMenu } from "react-icons/io5";
 import { IoCloseCircle } from "react-icons/io5";
 
 const Navbar = () => {
-  const[menu,Setmenu]=useState(true)
+  const[menu,Setmenu]=useState(false)
   const {getTotalCart}=useContext(Shopcontext)
   return (
-     <div className=' w-full h-20 bg-black text-white flex  md:gap-70 items-center cursor-pointer justify-center' >
+     <div className=' w-full  h-auto md:h-20 bg-black text-white flex  md:gap-70 items-center cursor-pointer justify-center' >
 
     
    
-      <div className='flex items-center gap-5'>
+      <div className='flex items-center md:gap-5'>
         <img src={logo} className='w-20 bg-black h-15'/>
-        <p className='font-bold text-3xl'>Shopshine</p>
-     <button className='md:hidden' onClick={()=>(!Setmenu)}>
-     {menu?  <IoMenu />:<IoCloseCircle />}
-</button>
+        <p className='font-bold md:text-3xl'>Shopshine</p>
+    
       </div>
 
       <div>
-        <ul className={`flex justify-center md:gap-8 md:text-lg ${menu?"block":"hidden"} `} >
+        <ul className={`md:flex justify-center md:gap-8 md:text-lg ${menu?"block":"hidden"} `} >
             <li onClick={()=>{Setmenu("home")}} > <Link to={"/shop"}> Home</Link>{menu==="home"?<hr/>:<></>}</li>
             <li onClick={()=>{Setmenu("mens")}}><Link to={"/mens"}> Mens</Link>{menu==="mens"?<hr/>:<></>}</li>
             <li onClick={()=>{Setmenu("women")}}><Link to={"/womens"}> womens</Link>{menu==="women"?<hr/>:<></>}</li>
             <li onClick={()=>{Setmenu("kids")}}><Link to={"/kids"}> Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
         </ul>
   </div>
-        <div className='flex items-center gap-10'>
+        <div className='flex items-center md:gap-10'>
            <Link to={"/login"}> <button className='bg-green-500 w-20 h-10 rounded-lg'> Login</button></Link>
            <div className='relative'>
             <p className='absolute -right-2 -top-2 w-6 rounded-full h-5 flex justify-center items-center font-semibold bg-red-500 text-white'> {getTotalCart()}</p> 
           <Link to={"/cart"}>  <GiShoppingCart className='w-10 bg-black h-10' /></Link>
           
+         
             </div>
+              <button className='md:hidden ' onClick={()=>Setmenu(!menu)}>
+     {menu?  <IoCloseCircle className='text-2xl'/>:<IoMenu className='text-2xl' />}
+</button>
         </div>
 
      
